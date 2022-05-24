f3382df7f948816bba75543ba3582c724027a83b
buyucuk <90260540+akaraol@users.noreply.github.com>
2022-05-24 20:10:37 +0300

    bismillahirrahmanirrahim

---
+++ app/src/main/java/com/akaraol/bottomnavtestchess/LoginScreenActivity.java
@@ -0,0 +1,104 @@
+package com.akaraol.bottomnavtestchess;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.akaraol.bottomnavtestchess.databinding.ActivityLoginScreenBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginScreenActivity extends AppCompatActivity {
+
+    private ActivityLoginScreenBinding binding;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginScreenBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(LoginScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+    }
+
+
+
+    public void signInClicked (View view) {
+        String email = binding.usernameInput.getText().toString();
+        String password = binding.pass.getText().toString();
+
+        if (email.equals("") || password.equals("")) {
+
+            Toast.makeText(this, "E-Mail veya Şifreniz Yanlış.", Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(LoginScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    Toast.makeText(LoginScreenActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+    }
+
+    public void signUpClicked (View view) {
+
+        String email = binding.usernameInput.getText().toString();
+        String password = binding.pass.getText().toString();
+
+        if (email.equals("") || password.equals("")) {
+
+            Toast.makeText(this, "Lütfen E-Mail ve Şifrenizi Girin.", Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+
+                    Intent intent = new Intent(LoginScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(LoginScreenActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+
+    }
+}
